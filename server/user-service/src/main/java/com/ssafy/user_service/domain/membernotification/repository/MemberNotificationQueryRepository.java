3d4b71f3831d8a95c5681344a33bbc653b7bd44d
Chaos0103 <lyt1228@naver.com>
2024-08-15 14:49:14 +0900

    feat(user-service): 회원의 알림 목록 조회 구현

--- server/user-service/src/main/java/com/ssafy/user_service/domain/membernotification/repository/MemberNotificationQueryRepository.java
+++ server/user-service/src/main/java/com/ssafy/user_service/domain/membernotification/repository/MemberNotificationQueryRepository.java
@@ -1,5 +1,7 @@
 package com.ssafy.user_service.domain.membernotification.repository;
 
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.ssafy.user_service.domain.membernotification.repository.response.NotificationResponse;
 import com.ssafy.user_service.domain.notification.NotificationCategory;
@@ -8,6 +10,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
+import static com.ssafy.user_service.domain.membernotification.QMemberNotification.memberNotification;
 
 @Repository
 public class MemberNotificationQueryRepository {
@@ -19,6 +23,30 @@ public class MemberNotificationQueryRepository {
     }
 
     public List<NotificationResponse> findAllByMemberKeyAndCond(String memberKey, NotificationCategory category, Pageable pageable) {
-        return null;
+        return queryFactory
+            .select(
+                Projections.fields(
+                    NotificationResponse.class,
+                    memberNotification.id,
+                    memberNotification.notification.notificationCategory.as("category"),
+                    memberNotification.notification.notificationContent.as("content"),
+                    memberNotification.isOpened,
+                    memberNotification.notification.createdDateTime.as("notificationDateTime")
+                )
+            )
+            .from(memberNotification)
+            .where(
+                memberNotification.isDeleted.isFalse(),
+                memberNotification.member.specificInfo.memberKey.eq(memberKey),
+                eqNotificationCategory(category)
+            )
+            .orderBy(memberNotification.createdDateTime.desc())
+            .limit(pageable.getPageSize())
+            .offset(pageable.getOffset())
+            .fetch();
+    }
+
+    private BooleanExpression eqNotificationCategory(NotificationCategory category) {
+        return category == null ? null : memberNotification.notification.notificationCategory.eq(category);
     }
 }
