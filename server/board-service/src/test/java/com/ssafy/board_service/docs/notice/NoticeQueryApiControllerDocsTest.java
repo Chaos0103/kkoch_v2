fa351e55896855e099771b691b66a759d56e75f5
Chaos0103 <lyt1228@naver.com>
2024-08-25 14:36:51 +0900

    test(board-service): 고정된 공지사항 목록 조회 API Docs 테스트 코드 구현

--- server/board-service/src/test/java/com/ssafy/board_service/docs/notice/NoticeQueryApiControllerDocsTest.java
+++ server/board-service/src/test/java/com/ssafy/board_service/docs/notice/NoticeQueryApiControllerDocsTest.java
@@ -3,6 +3,7 @@ package com.ssafy.board_service.docs.notice;
 import com.ssafy.board_service.api.PageResponse;
 import com.ssafy.board_service.api.controller.notice.NoticeQueryApiController;
 import com.ssafy.board_service.api.controller.notice.param.NoticeSearchParam;
+import com.ssafy.board_service.api.service.notice.FixedNoticeResponse;
 import com.ssafy.board_service.api.service.notice.NoticeQueryService;
 import com.ssafy.board_service.docs.RestDocsSupport;
 import com.ssafy.board_service.domain.notice.repository.response.NoticeResponse;
@@ -19,7 +20,8 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
@@ -65,7 +67,6 @@ class NoticeQueryApiControllerDocsTest extends RestDocsSupport {
             .andDo(print())
             .andExpect(status().isOk())
             .andDo(document("search-not-fixed-notices",
-                preprocessRequest(prettyPrint()),
                 preprocessResponse(prettyPrint()),
                 queryParameters(
                     parameterWithName("page")
@@ -102,6 +103,55 @@ class NoticeQueryApiControllerDocsTest extends RestDocsSupport {
                         .description("첫 페이지 여부"),
                     fieldWithPath("data.isLast").type(JsonFieldType.BOOLEAN)
                         .description("마지막 페이지 여부")
+                )
+            ));
+    }
+
+    @DisplayName("고정된 공지사항 목록 조회 API")
+    @Test
+    void searchFixedNotices() throws Exception {
+        NoticeResponse notice = NoticeResponse.builder()
+            .id(1)
+            .title("서버 긴급 점검 안내")
+            .isFixed(true)
+            .createdDateTime(LocalDateTime.of(2024, 8, 10, 10, 0, 0))
+            .build();
+
+        FixedNoticeResponse response = FixedNoticeResponse.builder()
+            .content(List.of(notice))
+            .build();
+
+        given(noticeQueryService.searchFixedNotices(any()))
+            .willReturn(response);
+
+        mockMvc.perform(
+                get("/board-service/notices/fixed")
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andDo(document("search-fixed-notices",
+                preprocessResponse(prettyPrint()),
+                responseFields(
+                    fieldWithPath("code").type(JsonFieldType.NUMBER)
+                        .description("코드"),
+                    fieldWithPath("status").type(JsonFieldType.STRING)
+                        .description("상태"),
+                    fieldWithPath("message").type(JsonFieldType.STRING)
+                        .description("메시지"),
+                    fieldWithPath("data").type(JsonFieldType.OBJECT)
+                        .description("응답 데이터"),
+                    fieldWithPath("data.content").type(JsonFieldType.ARRAY)
+                        .description("조회된 공지사항 목록"),
+                    fieldWithPath("data.content[].id").type(JsonFieldType.NUMBER)
+                        .description("공지사항 ID"),
+                    fieldWithPath("data.content[].title").type(JsonFieldType.STRING)
+                        .description("공지사항 제목"),
+                    fieldWithPath("data.content[].isFixed").type(JsonFieldType.BOOLEAN)
+                        .description("공지사항 고정 여부"),
+                    fieldWithPath("data.content[].createdDateTime").type(JsonFieldType.ARRAY)
+                        .description("공지사항 등록일시"),
+                    fieldWithPath("data.size").type(JsonFieldType.NUMBER)
+                        .description("조회된 데이터 갯수")
                 )
             ));
     }
