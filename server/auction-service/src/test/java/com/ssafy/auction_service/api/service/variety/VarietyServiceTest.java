9ad35d1700626d32a6f3c59a06f57504972c4952
Chaos0103 <lyt1228@naver.com>
2024-08-27 01:14:40 +0900

    test(auction-service): 품종 등록 테스트 코드 추가 구현

--- server/auction-service/src/test/java/com/ssafy/auction_service/api/service/variety/VarietyServiceTest.java
+++ server/auction-service/src/test/java/com/ssafy/auction_service/api/service/variety/VarietyServiceTest.java
@@ -6,6 +6,8 @@ import com.ssafy.auction_service.api.client.MemberServiceClient;
 import com.ssafy.auction_service.api.client.response.MemberIdResponse;
 import com.ssafy.auction_service.api.service.variety.request.VarietyCreateServiceRequest;
 import com.ssafy.auction_service.api.service.variety.response.VarietyCreateResponse;
+import com.ssafy.auction_service.common.exception.AppException;
+import com.ssafy.auction_service.domain.variety.PlantCategory;
 import com.ssafy.auction_service.domain.variety.Variety;
 import com.ssafy.auction_service.domain.variety.repository.VarietyRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -16,6 +18,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
 
 class VarietyServiceTest extends IntegrationTestSupport {
@@ -28,6 +31,36 @@ class VarietyServiceTest extends IntegrationTestSupport {
 
     @MockBean
     private MemberServiceClient memberServiceClient;
+
+    @DisplayName("이미 등록된 품종이라면 예외가 발생한다.")
+    @Test
+    void existVariety() {
+        //given
+        createVariety("10000001");
+
+        VarietyCreateServiceRequest request = VarietyCreateServiceRequest.builder()
+            .plantCategory("CUT_FLOWERS")
+            .itemName("장미")
+            .varietyName("하트앤소울")
+            .build();
+
+        MemberIdResponse memberId = MemberIdResponse.builder()
+            .memberId(1L)
+            .build();
+        ApiResponse<MemberIdResponse> apiResponse = ApiResponse.ok(memberId);
+
+        given(memberServiceClient.searchMemberId())
+            .willReturn(apiResponse);
+
+        //when
+        assertThatThrownBy(() -> varietyService.createVariety(request))
+            .isInstanceOf(AppException.class)
+            .hasMessage("이미 등록된 품종입니다.");
+
+        //then
+        List<Variety> varieties = varietyRepository.findAll();
+        assertThat(varieties).hasSize(1);
+    }
 
     @DisplayName("품종 정보를 입력 받아 품종을 등록한다.")
     @Test
@@ -59,5 +92,18 @@ class VarietyServiceTest extends IntegrationTestSupport {
 
         List<Variety> varieties = varietyRepository.findAll();
         assertThat(varieties).hasSize(1);
+    }
+
+    private Variety createVariety(String code) {
+        Variety variety = Variety.builder()
+            .isDeleted(false)
+            .createdBy(1L)
+            .lastModifiedBy(1L)
+            .code(code)
+            .plantCategory(PlantCategory.CUT_FLOWERS)
+            .itemName("장미")
+            .varietyName("하트앤소울")
+            .build();
+        return varietyRepository.save(variety);
     }
 }
