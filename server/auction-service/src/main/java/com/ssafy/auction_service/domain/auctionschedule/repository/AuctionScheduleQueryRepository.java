0ba7b7d9d31fb91552cbeaa44741e04659b553d6
Chaos0103 <lyt1228@naver.com>
2024-09-12 14:09:32 +0900

    feat(auction-service): 경매 일정 목록 조회 구현

--- server/auction-service/src/main/java/com/ssafy/auction_service/domain/auctionschedule/repository/AuctionScheduleQueryRepository.java
+++ server/auction-service/src/main/java/com/ssafy/auction_service/domain/auctionschedule/repository/AuctionScheduleQueryRepository.java
@@ -1,12 +1,19 @@
 package com.ssafy.auction_service.domain.auctionschedule.repository;
 
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.auction_service.domain.auctionschedule.AuctionStatus;
+import com.ssafy.auction_service.domain.auctionschedule.JointMarket;
 import com.ssafy.auction_service.domain.auctionschedule.repository.dto.AuctionScheduleSearchCond;
 import com.ssafy.auction_service.domain.auctionschedule.repository.response.AuctionScheduleResponse;
+import com.ssafy.auction_service.domain.variety.PlantCategory;
 import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
+import static com.ssafy.auction_service.domain.auctionschedule.QAuctionSchedule.auctionSchedule;
 
 @Repository
 public class AuctionScheduleQueryRepository {
@@ -18,6 +25,33 @@ public class AuctionScheduleQueryRepository {
     }
 
     public List<AuctionScheduleResponse> findByCond(AuctionScheduleSearchCond cond) {
-        return null;
+        return queryFactory
+            .select(
+                Projections.fields(
+                    AuctionScheduleResponse.class,
+                    auctionSchedule.id,
+                    auctionSchedule.auctionInfo.plantCategory,
+                    auctionSchedule.auctionInfo.jointMarket,
+                    auctionSchedule.auctionInfo.auctionStartDateTime,
+                    auctionSchedule.auctionStatus
+                )
+            )
+            .from(auctionSchedule)
+            .where(
+                auctionSchedule.isDeleted.isFalse(),
+                auctionSchedule.auctionStatus.in(AuctionStatus.getSearchableStatus()),
+                eqPlantCategory(cond.getPlantCategory()),
+                eqJointMarker(cond.getJointMarket())
+            )
+            .orderBy(auctionSchedule.auctionInfo.auctionStartDateTime.desc())
+            .fetch();
+    }
+
+    private BooleanExpression eqPlantCategory(PlantCategory plantCategory) {
+        return plantCategory == null ? null : auctionSchedule.auctionInfo.plantCategory.eq(plantCategory);
+    }
+
+    private BooleanExpression eqJointMarker(JointMarket jointMarket) {
+        return jointMarket == null ? null : auctionSchedule.auctionInfo.jointMarket.eq(jointMarket);
     }
 }
