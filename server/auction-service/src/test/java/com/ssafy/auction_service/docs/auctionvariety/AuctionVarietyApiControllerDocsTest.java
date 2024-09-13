2102b318a958e6f8574e3849821795d4cbe8e19d
Chaos0103 <lyt1228@naver.com>
2024-09-13 15:43:00 +0900

    test(auction-service): 경매 품종 수정 API Docs 테스트 코드 구현

--- server/auction-service/src/test/java/com/ssafy/auction_service/docs/auctionvariety/AuctionVarietyApiControllerDocsTest.java
+++ server/auction-service/src/test/java/com/ssafy/auction_service/docs/auctionvariety/AuctionVarietyApiControllerDocsTest.java
@@ -2,8 +2,10 @@ package com.ssafy.auction_service.docs.auctionvariety;
 
 import com.ssafy.auction_service.api.controller.auctionvariety.AuctionVarietyApiController;
 import com.ssafy.auction_service.api.controller.auctionvariety.request.AuctionVarietyCreateRequest;
+import com.ssafy.auction_service.api.controller.auctionvariety.request.AuctionVarietyModifyRequest;
 import com.ssafy.auction_service.api.service.auctionvariety.AuctionVarietyService;
 import com.ssafy.auction_service.api.service.auctionvariety.response.AuctionVarietyCreateResponse;
+import com.ssafy.auction_service.api.service.auctionvariety.response.AuctionVarietyModifyResponse;
 import com.ssafy.auction_service.docs.RestDocsSupport;
 import com.ssafy.auction_service.domain.auctionvariety.PlantGrade;
 import org.junit.jupiter.api.DisplayName;
@@ -20,8 +22,11 @@ import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -118,6 +123,76 @@ class AuctionVarietyApiControllerDocsTest extends RestDocsSupport {
                         .description("출하자"),
                     fieldWithPath("data.createdDateTime").type(JsonFieldType.ARRAY)
                         .description("경매 품종 등록일시")
+                )
+            ));
+    }
+
+    @DisplayName("경매 품종 수정 API")
+    @Test
+    void modifyAuctionSchedule() throws Exception {
+        AuctionVarietyModifyRequest request = AuctionVarietyModifyRequest.builder()
+            .plantGrade("ADVANCED")
+            .plantCount(15)
+            .auctionStartPrice(4000)
+            .build();
+
+        AuctionVarietyModifyResponse response = AuctionVarietyModifyResponse.builder()
+            .id(1L)
+            .listingNumber("00001")
+            .plantGrade(PlantGrade.ADVANCED)
+            .plantCount(15)
+            .auctionStartPrice(4000)
+            .build();
+
+        given(auctionVarietyService.modifyAuctionVariety(anyLong(), any()))
+            .willReturn(response);
+
+        mockMvc.perform(
+                patch("/auction-service/auction-varieties/{auctionVarietyId}", 1)
+                    .header(HttpHeaders.AUTHORIZATION, "issued.access.token")
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andDo(document("modify-auction-variety",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestHeaders(
+                    headerWithName(HttpHeaders.AUTHORIZATION)
+                        .description("회원 토큰")
+                ),
+                pathParameters(
+                    parameterWithName("auctionVarietyId")
+                        .description("경매 품종 ID")
+                ),
+                requestFields(
+                    fieldWithPath("plantGrade").type(JsonFieldType.STRING)
+                        .description("화훼등급"),
+                    fieldWithPath("plantCount").type(JsonFieldType.NUMBER)
+                        .description("화훼단수"),
+                    fieldWithPath("auctionStartPrice").type(JsonFieldType.NUMBER)
+                        .description("경매 시작가")
+                ),
+                responseFields(
+                    fieldWithPath("code").type(JsonFieldType.NUMBER)
+                        .description("코드"),
+                    fieldWithPath("status").type(JsonFieldType.STRING)
+                        .description("상태"),
+                    fieldWithPath("message").type(JsonFieldType.STRING)
+                        .description("메시지"),
+                    fieldWithPath("data").type(JsonFieldType.OBJECT)
+                        .description("응답 데이터"),
+                    fieldWithPath("data.id").type(JsonFieldType.NUMBER)
+                        .description("경매 품종 ID"),
+                    fieldWithPath("data.listingNumber").type(JsonFieldType.STRING)
+                        .description("경매번호"),
+                    fieldWithPath("data.plantGrade").type(JsonFieldType.STRING)
+                        .description("화훼등급"),
+                    fieldWithPath("data.plantCount").type(JsonFieldType.NUMBER)
+                        .description("화훼단수"),
+                    fieldWithPath("data.auctionStartPrice").type(JsonFieldType.NUMBER)
+                        .description("경매 시작가")
                 )
             ));
     }
