cd93759233b8fefb968b6d589014dd5c60967d66
Chaos0103 <lyt1228@naver.com>
2024-09-14 02:26:34 +0900

    feat(auction-service): 품종 목록 조회 구현

--- server/auction-service/src/main/java/com/ssafy/auction_service/api/service/variety/VarietyQueryService.java
+++ server/auction-service/src/main/java/com/ssafy/auction_service/api/service/variety/VarietyQueryService.java
@@ -1,12 +1,16 @@
 package com.ssafy.auction_service.api.service.variety;
 
 import com.ssafy.auction_service.api.PageResponse;
+import com.ssafy.auction_service.common.util.PageUtils;
 import com.ssafy.auction_service.domain.variety.repository.VarietyQueryRepository;
 import com.ssafy.auction_service.domain.variety.repository.cond.VarietySearchCond;
 import com.ssafy.auction_service.domain.variety.repository.response.VarietyResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -16,7 +20,12 @@ public class VarietyQueryService {
     private final VarietyQueryRepository varietyQueryRepository;
 
     public PageResponse<VarietyResponse> searchVarieties(VarietySearchCond cond, int pageNumber) {
-        return null;
-    }
+        Pageable pageable = PageUtils.of(pageNumber);
 
+        List<VarietyResponse> content = varietyQueryRepository.findAllByCond(cond, pageable);
+
+        int total = varietyQueryRepository.countByCond(cond);
+
+        return PageResponse.create(content, pageable, total);
+    }
 }
