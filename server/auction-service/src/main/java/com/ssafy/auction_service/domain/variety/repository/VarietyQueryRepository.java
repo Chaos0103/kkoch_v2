42c7a2ed75c44e2dd4fed88413e3c4d2bbf7b351
Chaos0103 <lyt1228@naver.com>
2024-09-14 02:09:18 +0900

    feat(auction-service): 품종 목록 조회 구현

--- server/auction-service/src/main/java/com/ssafy/auction_service/domain/variety/repository/VarietyQueryRepository.java
+++ server/auction-service/src/main/java/com/ssafy/auction_service/domain/variety/repository/VarietyQueryRepository.java
@@ -1,5 +1,7 @@
 package com.ssafy.auction_service.domain.variety.repository;
 
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPQLQueryFactory;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.ssafy.auction_service.domain.variety.repository.cond.VarietySearchCond;
@@ -9,6 +11,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
+import static com.ssafy.auction_service.common.util.StringUtils.isBlank;
+import static com.ssafy.auction_service.domain.variety.QVariety.variety;
 
 @Repository
 public class VarietyQueryRepository {
@@ -20,10 +25,36 @@ public class VarietyQueryRepository {
     }
 
     public List<VarietyResponse> findAllByCond(VarietySearchCond cond, Pageable pageable) {
-        return null;
+        return queryFactory
+            .select(
+                Projections.fields(
+                    VarietyResponse.class,
+                    variety.code,
+                    variety.info.plantCategory,
+                    variety.info.itemName,
+                    variety.info.varietyName
+                )
+            )
+            .from(variety)
+            .where(
+                variety.isDeleted.isFalse(),
+                variety.info.plantCategory.eq(cond.getPlantCategory()),
+                eqItemName(cond.getItemName())
+            )
+            .orderBy(
+                variety.info.itemName.asc(),
+                variety.info.varietyName.asc()
+            )
+            .limit(pageable.getPageSize())
+            .offset(pageable.getOffset())
+            .fetch();
     }
 
     public int countByCond(VarietySearchCond cond) {
         return 0;
+    }
+
+    private BooleanExpression eqItemName(String itemName) {
+        return isBlank(itemName) ? null : variety.info.itemName.eq(itemName);
     }
 }
