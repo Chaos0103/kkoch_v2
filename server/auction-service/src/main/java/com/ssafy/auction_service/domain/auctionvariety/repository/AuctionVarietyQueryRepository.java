c31b32a598b9bb81c96c7acbcdd73550d7b5af42
Chaos0103 <lyt1228@naver.com>
2024-09-13 18:22:05 +0900

    feat(auction-service): 경매 품종 목록 조회 구현

--- server/auction-service/src/main/java/com/ssafy/auction_service/domain/auctionvariety/repository/AuctionVarietyQueryRepository.java
+++ server/auction-service/src/main/java/com/ssafy/auction_service/domain/auctionvariety/repository/AuctionVarietyQueryRepository.java
@@ -1,5 +1,6 @@
 package com.ssafy.auction_service.domain.auctionvariety.repository;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.ssafy.auction_service.domain.auctionvariety.repository.response.AuctionVarietyResponse;
 import jakarta.persistence.EntityManager;
@@ -7,6 +8,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
+import static com.ssafy.auction_service.domain.auctionvariety.QAuctionVariety.auctionVariety;
+import static com.ssafy.auction_service.domain.variety.QVariety.variety;
 
 @Repository
 public class AuctionVarietyQueryRepository {
@@ -18,6 +22,32 @@ public class AuctionVarietyQueryRepository {
     }
 
     public List<AuctionVarietyResponse> findAllByAuctionScheduleId(int auctionScheduleId, Pageable pageable) {
-        return null;
+        return queryFactory
+            .select(
+                Projections.fields(
+                    AuctionVarietyResponse.class,
+                    auctionVariety.id,
+                    variety.code,
+                    variety.info.plantCategory,
+                    variety.info.itemName,
+                    variety.info.varietyName,
+                    auctionVariety.listingNumber,
+                    auctionVariety.auctionPlant.plantGrade,
+                    auctionVariety.auctionPlant.plantCount,
+                    auctionVariety.auctionPlant.auctionStartPrice.value,
+                    auctionVariety.shipment.region,
+                    auctionVariety.shipment.shipper
+                )
+            )
+            .from(auctionVariety)
+            .join(auctionVariety.variety, variety)
+            .where(
+                auctionVariety.isDeleted.isFalse(),
+                auctionVariety.auctionSchedule.id.eq(auctionScheduleId)
+            )
+            .orderBy(auctionVariety.listingNumber.asc())
+            .limit(pageable.getPageSize())
+            .offset(pageable.getOffset())
+            .fetch();
     }
 }
