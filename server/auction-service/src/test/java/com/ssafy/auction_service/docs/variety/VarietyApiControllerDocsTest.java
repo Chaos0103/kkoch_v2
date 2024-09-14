6b652e53cba53100455e8d8f2adc4787f3381390
Chaos0103 <lyt1228@naver.com>
2024-09-14 20:38:00 +0900

    test(auction-service): 품종 삭제 API Docs 테스트 코드 구현

--- server/auction-service/src/test/java/com/ssafy/auction_service/docs/variety/VarietyApiControllerDocsTest.java
+++ server/auction-service/src/test/java/com/ssafy/auction_service/docs/variety/VarietyApiControllerDocsTest.java
@@ -6,6 +6,7 @@ import com.ssafy.auction_service.api.controller.variety.request.VarietyModifyReq
 import com.ssafy.auction_service.api.service.variety.VarietyService;
 import com.ssafy.auction_service.api.service.variety.response.VarietyCreateResponse;
 import com.ssafy.auction_service.api.service.variety.response.VarietyModifyResponse;
+import com.ssafy.auction_service.api.service.variety.response.VarietyRemoveResponse;
 import com.ssafy.auction_service.docs.RestDocsSupport;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,6 +23,7 @@ import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
@@ -166,6 +168,57 @@ class VarietyApiControllerDocsTest extends RestDocsSupport {
                         .description("품종명"),
                     fieldWithPath("data.modifiedDateTime").type(JsonFieldType.ARRAY)
                         .description("품종 수정일시")
+                )
+            ));
+    }
+
+    @DisplayName("품종 삭제 API")
+    @Test
+    void removeVariety() throws Exception {
+        VarietyRemoveResponse response = VarietyRemoveResponse.builder()
+            .code("10031204")
+            .plantCategory("절화")
+            .itemName("장미")
+            .varietyName("하트앤소울")
+            .build();
+
+        given(varietyService.removeVariety(anyString()))
+            .willReturn(response);
+
+        mockMvc.perform(
+                delete("/auction-service/varieties/{varietyCode}", "10031204")
+                    .header(HttpHeaders.AUTHORIZATION, "issued.access.token")
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andDo(document("remove-variety",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestHeaders(
+                    headerWithName(HttpHeaders.AUTHORIZATION)
+                        .description("회원 토큰")
+                ),
+                pathParameters(
+                    parameterWithName("varietyCode")
+                        .description("품종코드")
+                ),
+                responseFields(
+                    fieldWithPath("code").type(JsonFieldType.NUMBER)
+                        .description("코드"),
+                    fieldWithPath("status").type(JsonFieldType.STRING)
+                        .description("상태"),
+                    fieldWithPath("message").type(JsonFieldType.STRING)
+                        .description("메시지"),
+                    fieldWithPath("data").type(JsonFieldType.OBJECT)
+                        .description("응답 데이터"),
+                    fieldWithPath("data.code").type(JsonFieldType.STRING)
+                        .description("품종코드"),
+                    fieldWithPath("data.plantCategory").type(JsonFieldType.STRING)
+                        .description("화훼부류"),
+                    fieldWithPath("data.itemName").type(JsonFieldType.STRING)
+                        .description("품목명"),
+                    fieldWithPath("data.varietyName").type(JsonFieldType.STRING)
+                        .description("품종명")
                 )
             ));
     }
