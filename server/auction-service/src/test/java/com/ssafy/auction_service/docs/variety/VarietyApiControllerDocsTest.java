b58b367dce174912715d720cb9d7099b160ca81f
Chaos0103 <lyt1228@naver.com>
2024-09-14 19:58:58 +0900

    test(auction-service): 품종 수정 API Docs 테스트 코드 구현

--- server/auction-service/src/test/java/com/ssafy/auction_service/docs/variety/VarietyApiControllerDocsTest.java
+++ server/auction-service/src/test/java/com/ssafy/auction_service/docs/variety/VarietyApiControllerDocsTest.java
@@ -2,8 +2,10 @@ package com.ssafy.auction_service.docs.variety;
 
 import com.ssafy.auction_service.api.controller.variety.VarietyApiController;
 import com.ssafy.auction_service.api.controller.variety.request.VarietyCreateRequest;
+import com.ssafy.auction_service.api.controller.variety.request.VarietyModifyRequest;
 import com.ssafy.auction_service.api.service.variety.VarietyService;
 import com.ssafy.auction_service.api.service.variety.response.VarietyCreateResponse;
+import com.ssafy.auction_service.api.service.variety.response.VarietyModifyResponse;
 import com.ssafy.auction_service.docs.RestDocsSupport;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,13 +16,17 @@ import org.springframework.restdocs.payload.JsonFieldType;
 import java.time.LocalDateTime;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -96,6 +102,70 @@ class VarietyApiControllerDocsTest extends RestDocsSupport {
                         .description("품종명"),
                     fieldWithPath("data.createdDateTime").type(JsonFieldType.ARRAY)
                         .description("품종 등록일시")
+                )
+            ));
+    }
+
+    @DisplayName("품종 수정 API")
+    @Test
+    void modifyVariety() throws Exception {
+        VarietyModifyRequest request = VarietyModifyRequest.builder()
+            .varietyName("하젤")
+            .build();
+
+        VarietyModifyResponse response = VarietyModifyResponse.builder()
+            .code("10031204")
+            .plantCategory("절화")
+            .itemName("장미")
+            .varietyName("하젤")
+            .modifiedDateTime(LocalDateTime.now())
+            .build();
+
+        given(varietyService.modifyVariety(anyString(), anyString(), any()))
+            .willReturn(response);
+
+        mockMvc.perform(
+                patch("/auction-service/varieties/{varietyCode}", "10031204")
+                    .header(HttpHeaders.AUTHORIZATION, "issued.access.token")
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andDo(document("modify-variety",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestHeaders(
+                    headerWithName(HttpHeaders.AUTHORIZATION)
+                        .description("회원 토큰")
+                ),
+                pathParameters(
+                    parameterWithName("varietyCode")
+                        .description("품종코드")
+                ),
+                requestFields(
+                    fieldWithPath("varietyName").type(JsonFieldType.STRING)
+                        .description("품종명")
+                ),
+                responseFields(
+                    fieldWithPath("code").type(JsonFieldType.NUMBER)
+                        .description("코드"),
+                    fieldWithPath("status").type(JsonFieldType.STRING)
+                        .description("상태"),
+                    fieldWithPath("message").type(JsonFieldType.STRING)
+                        .description("메시지"),
+                    fieldWithPath("data").type(JsonFieldType.OBJECT)
+                        .description("응답 데이터"),
+                    fieldWithPath("data.code").type(JsonFieldType.STRING)
+                        .description("품종코드"),
+                    fieldWithPath("data.plantCategory").type(JsonFieldType.STRING)
+                        .description("화훼부류"),
+                    fieldWithPath("data.itemName").type(JsonFieldType.STRING)
+                        .description("품목명"),
+                    fieldWithPath("data.varietyName").type(JsonFieldType.STRING)
+                        .description("품종명"),
+                    fieldWithPath("data.modifiedDateTime").type(JsonFieldType.ARRAY)
+                        .description("품종 수정일시")
                 )
             ));
     }
