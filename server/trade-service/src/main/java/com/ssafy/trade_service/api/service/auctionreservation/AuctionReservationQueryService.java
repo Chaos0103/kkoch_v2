03220cf71df152e9cce5a746c1dc4e7aeb7936c9
Chaos0103 <lyt1228@naver.com>
2024-09-17 01:21:03 +0900

    feat(trade-service): 경매 예약 목록 조회 구현

--- server/trade-service/src/main/java/com/ssafy/trade_service/api/service/auctionreservation/AuctionReservationQueryService.java
+++ server/trade-service/src/main/java/com/ssafy/trade_service/api/service/auctionreservation/AuctionReservationQueryService.java
@@ -1,12 +1,16 @@
 package com.ssafy.trade_service.api.service.auctionreservation;
 
+import com.ssafy.trade_service.api.ApiResponse;
 import com.ssafy.trade_service.api.ListResponse;
 import com.ssafy.trade_service.api.client.MemberServiceClient;
+import com.ssafy.trade_service.api.client.response.MemberIdResponse;
 import com.ssafy.trade_service.domain.auctionreservation.repository.AuctionReservationQueryRepository;
 import com.ssafy.trade_service.domain.auctionreservation.repository.response.AuctionReservationResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -17,6 +21,15 @@ public class AuctionReservationQueryService {
     private final MemberServiceClient memberServiceClient;
 
     public ListResponse<AuctionReservationResponse> searchAuctionReservations(int auctionScheduleId) {
-        return null;
+        Long memberId = getMemberId();
+
+        List<AuctionReservationResponse> content = auctionReservationQueryRepository.findAllByAuctionScheduleId(memberId, auctionScheduleId);
+
+        return ListResponse.of(content);
+    }
+
+    private Long getMemberId() {
+        ApiResponse<MemberIdResponse> response = memberServiceClient.searchMemberId();
+        return response.getData().getMemberId();
     }
 }
