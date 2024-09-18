831fce30dbf8a03d9d338101a6257741cd5fd8e1
Chaos0103 <lyt1228@naver.com>
2024-09-18 14:33:10 +0900

    feat(trade-service): 주문 목록 조회 API 구현

--- server/trade-service/src/main/java/com/ssafy/trade_service/api/controller/order/OrderApiQueryController.java
+++ server/trade-service/src/main/java/com/ssafy/trade_service/api/controller/order/OrderApiQueryController.java
@@ -1,8 +1,10 @@
 package com.ssafy.trade_service.api.controller.order;
 
 import com.ssafy.trade_service.api.ApiResponse;
+import com.ssafy.trade_service.api.PageResponse;
 import com.ssafy.trade_service.api.controller.order.param.OrderSearchParam;
 import com.ssafy.trade_service.api.service.order.OrderQueryService;
+import com.ssafy.trade_service.common.util.PageUtils;
 import com.ssafy.trade_service.domain.order.repository.response.OrderResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,7 +21,11 @@ public class OrderApiQueryController {
     private final OrderQueryService orderQueryService;
 
     @GetMapping
-    public ApiResponse<OrderResponse> searchOrders(@Valid @ModelAttribute OrderSearchParam param) {
-        return null;
+    public ApiResponse<PageResponse<OrderResponse>> searchOrders(@Valid @ModelAttribute OrderSearchParam param) {
+        int pageNumber = PageUtils.parsePageNumber(param.getPage());
+
+        PageResponse<OrderResponse> response = orderQueryService.searchOrders(pageNumber);
+
+        return ApiResponse.ok(response);
     }
 }
