97affa365c2af9f8438bb8ff71f626ede0bcf886
Chaos0103 <lyt1228@naver.com>
2024-09-18 14:20:14 +0900

    feat(trade-service): 주문 목록 조회 구현

--- server/trade-service/src/main/java/com/ssafy/trade_service/api/service/order/OrderQueryService.java
+++ server/trade-service/src/main/java/com/ssafy/trade_service/api/service/order/OrderQueryService.java
@@ -1,11 +1,18 @@
 package com.ssafy.trade_service.api.service.order;
 
+import com.ssafy.trade_service.api.ApiResponse;
 import com.ssafy.trade_service.api.PageResponse;
+import com.ssafy.trade_service.api.client.MemberServiceClient;
+import com.ssafy.trade_service.api.client.response.MemberIdResponse;
+import com.ssafy.trade_service.common.util.PageUtils;
 import com.ssafy.trade_service.domain.order.repository.OrderQueryRepository;
 import com.ssafy.trade_service.domain.order.repository.response.OrderResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -13,8 +20,21 @@ import org.springframework.transaction.annotation.Transactional;
 public class OrderQueryService {
 
     private final OrderQueryRepository orderQueryRepository;
+    private final MemberServiceClient memberServiceClient;
 
     public PageResponse<OrderResponse> searchOrders(int pageNumber) {
-        return null;
+        Long memberId = getMemberId();
+
+        Pageable pageable = PageUtils.of(pageNumber);
+
+        List<OrderResponse> content = orderQueryRepository.findAllByMemberId(memberId, pageable);
+        int total = orderQueryRepository.countByMemberId(memberId);
+
+        return PageResponse.create(content, pageable, total);
+    }
+
+    private Long getMemberId() {
+        ApiResponse<MemberIdResponse> response = memberServiceClient.searchMemberId();
+        return response.getData().getMemberId();
     }
 }
