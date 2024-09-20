4af711fef13e675192db91f241d58b2d7bc39a29
Chaos0103 <lyt1228@naver.com>
2024-09-20 23:58:18 +0900

    feat(trade-service): 결제 API 구현

--- server/trade-service/src/main/java/com/ssafy/trade_service/api/controller/payment/PaymentApiController.java
+++ server/trade-service/src/main/java/com/ssafy/trade_service/api/controller/payment/PaymentApiController.java
@@ -3,11 +3,14 @@ package com.ssafy.trade_service.api.controller.payment;
 import com.ssafy.trade_service.api.ApiResponse;
 import com.ssafy.trade_service.api.service.payment.PaymentCreateResponse;
 import com.ssafy.trade_service.api.service.payment.PaymentService;
+import com.ssafy.trade_service.common.util.TimeUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +21,10 @@ public class PaymentApiController {
 
     @PostMapping
     public ApiResponse<PaymentCreateResponse> payment(@PathVariable Long orderId) {
-        return null;
+        LocalDateTime current = TimeUtils.getCurrentDateTime();
+
+        PaymentCreateResponse response = paymentService.payment(orderId, current);
+
+        return ApiResponse.ok(response);
     }
 }
