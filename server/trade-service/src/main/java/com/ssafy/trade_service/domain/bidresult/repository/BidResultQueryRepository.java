2b291447861bb35d17fcef2846d0e8c405efb38d
Chaos0103 <lyt1228@naver.com>
2024-09-23 04:20:38 +0900

    feat(trade-service): 낙찰 결과 목록 조회 구현

--- server/trade-service/src/main/java/com/ssafy/trade_service/domain/bidresult/repository/BidResultQueryRepository.java
+++ server/trade-service/src/main/java/com/ssafy/trade_service/domain/bidresult/repository/BidResultQueryRepository.java
@@ -1,11 +1,14 @@
 package com.ssafy.trade_service.domain.bidresult.repository;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.ssafy.trade_service.domain.bidresult.repository.dto.BidResultDto;
 import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
+import static com.ssafy.trade_service.domain.bidresult.QBidResult.bidResult;
 
 @Repository
 public class BidResultQueryRepository {
@@ -17,6 +20,22 @@ public class BidResultQueryRepository {
     }
 
     public List<BidResultDto> findAllByOrderId(Long orderId) {
-        return null;
+        return queryFactory
+            .select(
+                Projections.fields(
+                    BidResultDto.class,
+                    bidResult.id,
+                    bidResult.bidPrice,
+                    bidResult.bidDateTime,
+                    bidResult.auctionVarietyId
+                )
+            )
+            .from(bidResult)
+            .where(
+                bidResult.isDeleted.isFalse(),
+                bidResult.order.id.eq(orderId)
+            )
+            .orderBy(bidResult.bidDateTime.asc())
+            .fetch();
     }
 }
