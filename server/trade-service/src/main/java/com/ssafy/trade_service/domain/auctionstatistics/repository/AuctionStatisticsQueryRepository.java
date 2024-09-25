e54dbec224e94d17776b07c9c5d5e8359f4a7e78
Chaos0103 <lyt1228@naver.com>
2024-09-25 14:47:16 +0900

    feat(trade-service): 경매 통계 목록 조회 구현

--- server/trade-service/src/main/java/com/ssafy/trade_service/domain/auctionstatistics/repository/AuctionStatisticsQueryRepository.java
+++ server/trade-service/src/main/java/com/ssafy/trade_service/domain/auctionstatistics/repository/AuctionStatisticsQueryRepository.java
@@ -1,5 +1,7 @@
 package com.ssafy.trade_service.domain.auctionstatistics.repository;
 
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.ssafy.trade_service.domain.auctionstatistics.repository.cond.AuctionStatisticsSearchCond;
 import com.ssafy.trade_service.domain.auctionstatistics.repository.response.AuctionStatisticsResponse;
@@ -7,6 +9,9 @@ import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
+import static com.ssafy.trade_service.domain.auctionstatistics.QAuctionStatistics.auctionStatistics;
+import static org.springframework.util.StringUtils.hasText;
 
 @Repository
 public class AuctionStatisticsQueryRepository {
@@ -18,6 +23,30 @@ public class AuctionStatisticsQueryRepository {
     }
 
     public List<AuctionStatisticsResponse> findAllByCond(String varietyCode, AuctionStatisticsSearchCond cond) {
-        return null;
+        return queryFactory
+            .select(
+                Projections.fields(
+                    AuctionStatisticsResponse.class,
+                    auctionStatistics.varietyCode,
+                    auctionStatistics.plantGrade,
+                    auctionStatistics.plantCount,
+                    auctionStatistics.calculatedResult.avg,
+                    auctionStatistics.calculatedResult.max,
+                    auctionStatistics.calculatedResult.min,
+                    auctionStatistics.calculatedDate
+                )
+            )
+            .from(auctionStatistics)
+            .where(
+                auctionStatistics.varietyCode.eq(varietyCode),
+                auctionStatistics.calculatedDate.between(cond.getFrom(), cond.getTo()),
+                eqPlantGrade(cond.getPlantGrade())
+            )
+            .orderBy(auctionStatistics.calculatedDate.desc())
+            .fetch();
+    }
+
+    private BooleanExpression eqPlantGrade(String plantGrade) {
+        return hasText(plantGrade) ? auctionStatistics.plantGrade.eq(plantGrade) : null;
     }
 }
