300edbbb929e32f104c6b2873ee269b12cfe1ab6
Chaos0103 <lyt1228@naver.com>
2024-09-25 14:51:30 +0900

    test(trade-service): 경매 통계 목록 조회 테스트 코드 구현

--- server/trade-service/src/main/java/com/ssafy/trade_service/api/service/auctionstatistics/AuctionStatisticsQueryService.java
+++ server/trade-service/src/main/java/com/ssafy/trade_service/api/service/auctionstatistics/AuctionStatisticsQueryService.java
@@ -1,5 +1,9 @@
 package com.ssafy.trade_service.api.service.auctionstatistics;
 
+import com.ssafy.trade_service.api.ListResponse;
+import com.ssafy.trade_service.domain.auctionstatistics.repository.AuctionStatisticsQueryRepository;
+import com.ssafy.trade_service.domain.auctionstatistics.repository.cond.AuctionStatisticsSearchCond;
+import com.ssafy.trade_service.domain.auctionstatistics.repository.response.AuctionStatisticsResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,4 +12,10 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @RequiredArgsConstructor
 public class AuctionStatisticsQueryService {
+
+    private final AuctionStatisticsQueryRepository auctionStatisticsQueryRepository;
+
+    public final ListResponse<AuctionStatisticsResponse> searchAuctionStatistics(String varietyCode, AuctionStatisticsSearchCond cond) {
+        return null;
+    }
 }
